8410a0d1bd3e1e0cfbee12b7f72be4bb0f1d2cdb
Hui Xiao <huixiao@fb.com>
2022-09-27 20:20:33 -0700

    revert

--- db/db_flush_test.cc
+++ db/db_flush_test.cc
@@ -2378,60 +2378,60 @@ TEST_P(DBFlushTestBlobError, FlushError) {
 }
 
 #ifndef ROCKSDB_LITE
-TEST_F(DBFlushTest, TombstoneVisibleInSnapshot) {
-  class SimpleTestFlushListener : public EventListener {
-   public:
-    explicit SimpleTestFlushListener(DBFlushTest* _test) : test_(_test) {}
-    ~SimpleTestFlushListener() override {}
+// TEST_F(DBFlushTest, TombstoneVisibleInSnapshot) {
+//   class SimpleTestFlushListener : public EventListener {
+//    public:
+//     explicit SimpleTestFlushListener(DBFlushTest* _test) : test_(_test) {}
+//     ~SimpleTestFlushListener() override {}
 
-    void OnFlushBegin(DB* db, const FlushJobInfo& info) override {
-      ASSERT_EQ(static_cast<uint32_t>(0), info.cf_id);
+//     void OnFlushBegin(DB* db, const FlushJobInfo& info) override {
+//       ASSERT_EQ(static_cast<uint32_t>(0), info.cf_id);
 
-      ASSERT_OK(db->Delete(WriteOptions(), "foo"));
-      snapshot_ = db->GetSnapshot();
-      ASSERT_OK(db->Put(WriteOptions(), "foo", "value"));
+//       ASSERT_OK(db->Delete(WriteOptions(), "foo"));
+//       snapshot_ = db->GetSnapshot();
+//       ASSERT_OK(db->Put(WriteOptions(), "foo", "value"));
 
-      auto* dbimpl = static_cast_with_check<DBImpl>(db);
-      assert(dbimpl);
+//       auto* dbimpl = static_cast_with_check<DBImpl>(db);
+//       assert(dbimpl);
 
-      ColumnFamilyHandle* cfh = db->DefaultColumnFamily();
-      auto* cfhi = static_cast_with_check<ColumnFamilyHandleImpl>(cfh);
-      assert(cfhi);
-      ASSERT_OK(dbimpl->TEST_SwitchMemtable(cfhi->cfd()));
-    }
+//       ColumnFamilyHandle* cfh = db->DefaultColumnFamily();
+//       auto* cfhi = static_cast_with_check<ColumnFamilyHandleImpl>(cfh);
+//       assert(cfhi);
+//       ASSERT_OK(dbimpl->TEST_SwitchMemtable(cfhi->cfd()));
+//     }
 
-    DBFlushTest* test_ = nullptr;
-    const Snapshot* snapshot_ = nullptr;
-  };
+//     DBFlushTest* test_ = nullptr;
+//     const Snapshot* snapshot_ = nullptr;
+//   };
 
-  Options options = CurrentOptions();
-  options.create_if_missing = true;
-  auto* listener = new SimpleTestFlushListener(this);
-  options.listeners.emplace_back(listener);
-  DestroyAndReopen(options);
+//   Options options = CurrentOptions();
+//   options.create_if_missing = true;
+//   auto* listener = new SimpleTestFlushListener(this);
+//   options.listeners.emplace_back(listener);
+//   DestroyAndReopen(options);
 
-  ASSERT_OK(db_->Put(WriteOptions(), "foo", "value0"));
+//   ASSERT_OK(db_->Put(WriteOptions(), "foo", "value0"));
 
-  ManagedSnapshot snapshot_guard(db_);
+//   ManagedSnapshot snapshot_guard(db_);
 
-  ColumnFamilyHandle* default_cf = db_->DefaultColumnFamily();
-  ASSERT_OK(db_->Flush(FlushOptions(), default_cf));
+//   ColumnFamilyHandle* default_cf = db_->DefaultColumnFamily();
+//   ASSERT_OK(db_->Flush(FlushOptions(), default_cf));
 
-  const Snapshot* snapshot = listener->snapshot_;
-  assert(snapshot);
+//   const Snapshot* snapshot = listener->snapshot_;
+//   assert(snapshot);
 
-  ReadOptions read_opts;
-  read_opts.snapshot = snapshot;
+//   ReadOptions read_opts;
+//   read_opts.snapshot = snapshot;
 
-  // Using snapshot should not see "foo".
-  {
-    std::string value;
-    Status s = db_->Get(read_opts, "foo", &value);
-    ASSERT_TRUE(s.IsNotFound());
-  }
+//   // Using snapshot should not see "foo".
+//   {
+//     std::string value;
+//     Status s = db_->Get(read_opts, "foo", &value);
+//     ASSERT_TRUE(s.IsNotFound());
+//   }
 
-  db_->ReleaseSnapshot(snapshot);
-}
+//   db_->ReleaseSnapshot(snapshot);
+// }
 
 TEST_P(DBAtomicFlushTest, ManualFlushUnder2PC) {
   Options options = CurrentOptions();
